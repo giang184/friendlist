e55e673ce643ccd20624d23909fc4d5d33beba9b
Andrew Giang <giang184@gmail.com>
2021-10-21 16:41:09 -0700

    update about page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ApplicationController
   def index
   end
   def about
+    @about = "This site is a simple web-app that will let you create and store a contact list."
   end
 end
